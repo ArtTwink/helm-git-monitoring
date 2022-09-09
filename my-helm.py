720c5823601704a12fc4ee97bbd0caa77800e88c
Artur Mokhov <amokhov@innoseti.ru>
2022-09-09 14:18:18 +0300

    finding filenames

--- my-helm.py
+++ my-helm.py
@@ -1,23 +1,24 @@
+from distutils.filelist import findall
 import re
 import subprocess
 from itertools import groupby
+from xml.etree.ElementTree import Comment
 
 output = subprocess.getoutput("helm get all -n default car")
-print(type(output))
+# print(type(output))
 encode1 = output.partition('MANIFEST:') [-1]
-print("encode1 :", encode1, type(encode1))
-# encode2 = encode1.partition('\n')
-# helm_cut = re.split('MANIFEST: ', output)
-# print(helm_cut)
-manifests = re.split('#', output)
-
-# Name of files
-# for i in manifests:
-#     print(i, sep=",,,,,,,,,")
-# x = re.findall
-
+# print("encode1 :", encode1, type(encode1))
+# name_manifests = 
+# manifests = re.split('#', output)
 
 with open('data.txt', 'w') as f:
-    for item in manifests:
-        f.write("%s\n" % item.split(','))
+    for item in encode1:
+        f.write(item)
+        # f.write("%s\n" % item.split(','))
     print('Done')
+print(encode1)
+comment = re.findall(r'#.+', encode1)
+print(comment) 
+# print(encode1.split('#'), sep=',')
+# for i in list(encode1):
+#     print(i)
