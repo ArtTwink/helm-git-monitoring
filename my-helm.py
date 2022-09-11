e86e39eb34db8493787a29441b1e441be4c77d1f
arttwink <rthur.s8@gmail.com>
2022-09-11 23:13:02 +0200

    fixed file_name, working on namespace_name

--- my-helm.py
+++ my-helm.py
@@ -1,24 +1,32 @@
 from distutils.filelist import findall
+import os
 import re
 import subprocess
-from itertools import groupby
-from xml.etree.ElementTree import Comment
+
 
 output = subprocess.getoutput("helm get all -n default car")
 # print(type(output))
-encode1 = output.partition('MANIFEST:') [-1]
-# print("encode1 :", encode1, type(encode1))
+cut_output = output.partition('MANIFEST:') [-1]
+# print("cut_output :", cut_output, type(cut_output))
 # name_manifests = 
 # manifests = re.split('#', output)
 
 with open('data.txt', 'w') as f:
-    for item in encode1:
+    for item in cut_output:
         f.write(item)
         # f.write("%s\n" % item.split(','))
     # print('Done')
-# print(encode1)
-helm_comment = re.findall(r'#.+', encode1)
+# print(cut_output)
+helm_comment = re.findall(r'#.+', cut_output)
 # print(type(helm_comment))
+# print(helm_comment)
+
+# Namespace
+namespace_name = []
+# print(output)
+# namespace_name.append(re.findall('NAMESPACE: (.*?)$', output).group(1))
+print(re.search('NAMESPACE: (.*?)$', output))
+print(namespace_name)
 
 # Name
 chart_name = []
@@ -35,5 +43,12 @@ print(essential_name)
 # File name
 file_name = []
 for i in helm_comment:
-    file_name.append(re.search('\/.*', i))
+    file_name.append(re.search('/.*?/(.*?)$', i).group(1))
 print(file_name)
+
+# Creating files
+for i in chart_name:
+    os.mkdir(i)
+    create_name = os.path.join(i, file_name)
+    with open(i, 'wt') as f:
+        f.write('example')
